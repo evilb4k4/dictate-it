196e7a6b43c7deef533a8e1c623aab54596d18a9
Ryan Turner <ryanthepit@gmail.com>
2017-08-13 17:43:44 -0700

    added auth-router

---
+++ backend/src/routes/auth-router.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const {Router} = require('express');
+const jsonParser = require('body-parser').json();
+const User = require('../model/user.js');
+
+const authRouter = module.exports = new Router();
+
+const basicAuth = require('../lib/basic-auth-middleware.js');
+
+authRouter.post('/api/auth/register', jsonParser, (req, res, next) => {
+  console.log('body', req.body);
+  if(!req.body.password || !req.body.username) {
+    return next(new Error('required arguments'));
+  }
+
+  User.create(req.body)
+    .then(token => res.send(token))
+    .catch(next);
+});
+
+authRouter.get('/api/auth/login', basicAuth, (req, res, next) => {
+  console.log('api/auth/login');
+  req.user.tokenCreate()
+    .then(token => res.send(token))
+    .catch(next);
+});
+
+authRouter.delete('api/auth/delete', bearerAuth, (req, res, next) => {
+  console.log('Hit DELETE', req.user);
+  User.findOneAndRemove({_id: req.user._id})
+    .then(user => {
+      res.send(user._id);
+    })
+    .catch(next);
+});
