e91533b55f34aee03d2a8cfaf410e48123f8abe2
Ryan Turner <ryanthepit@gmail.com>
2017-08-13 18:42:35 -0700

    fixed error in user model

---
+++ backend/src/lib/mongo.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+
+const state = {
+  isOn: false,
+  config: {
+    useMongoClient: true,
+    promiseLibrary: Promise,
+  },
+};
+
+export const start = () => {
+  if(state.isOn)
+    return Promise.reject(new Error('USER ERROR: db is connected'));
+  return mongoose.connect(process.env.MONGODB_URI, state.config)
+    .then(() => {
+      console.log('__MONGO_CONNECTED__', process.env.MONGODB_URI);
+      state.isOn = true;
+    });
+};
+
+export const stop = () => {
+  if(!state.isOn)
+    return Promise.reject(new Error('USER ERROR: db is disconnected'));
+  return mongoose.disconnect()
+    .then(() => {
+      state.isOn = false;
+      console.log('__MONGO_DISCONNECTED__');
+    });
+};
