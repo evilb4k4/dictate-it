bdcc102289d550130dc0c3bf18d54f47c6a537b9
adan.rabadan <evilb4k4@gmail.com>
2017-08-14 11:33:30 -0700

    added the app component and webpack

--- frontend/src/reducer/route.js
+++ frontend/src/reducer/route.js
@@ -1,13 +1,13 @@
 
 export default (state='/landing', {type, payload}) => {
   switch(type){
-    case 'LOGIN':
-      return '/chat'
-    case 'LOGOUT':
-      return '/landing'
-    case 'SWITCH_ROUTE':
-      return payload
-    default:
-      return state
+  case 'LOGIN':
+    return '/chat';
+  case 'LOGOUT':
+    return '/landing';
+  case 'SWITCH_ROUTE':
+    return payload;
+  default:
+    return state;
   }
-}
+};
