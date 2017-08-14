7a90707eed208acda0f12fb0924a9d7077b9c754
adan.rabadan <evilb4k4@gmail.com>
2017-08-14 15:58:30 -0700

    added a ditation and statement reducer

---
+++ frontend/src/main.js
@@ -0,0 +1,20 @@
+import './style/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './component/app';
+import {Provider} from 'react-redux';
+import storeCreate from './lib/store-create';
+import io from './lib/io.js';
+// server subscribers
+import userSubscribers from './subscribe/user.js';
+const store = storeCreate();
+// combine the subscribers
+let subscribers = Object.assign(userSubscribers);
+// add them to the io module
+io(store, subscribers);
+let AppContainer = () => (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+);
+ReactDom.render( <AppContainer/> , document.getElementById('root'));
