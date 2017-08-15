a0164c873ba065ede5efb1e648d3e55c53683374
Izabella Baer <izabellabaer@gmail.com>
2017-08-15 15:31:15 -0700

    added dictation action

---
+++ src/component/app/dictation/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import Listener from '../listener';
+
+export default class Dictation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+    };
+  }
+
+  render() {
+    return (
+      <div className="live-dictation">
+        <h2>{this.state.title}</h2>
+        <p>{this.state.description}</p>
+        <Listener />
+      </div>
+    );
+  }
+}
