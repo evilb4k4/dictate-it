4336fc44ee2330b6ce587aa2f4ba8eae90e87a44
adan.rabadan <evilb4k4@gmail.com>
2017-08-15 16:31:44 -0700

    added dictation-reducer test

--- src/__test__/statement-action.test.js
+++ src/__test__/statement-action.test.js
@@ -8,7 +8,6 @@ describe('testing statement actions', () => {
   test('statementCreate returns a STATEMENT_CREATE action', () => {
     let action = statementCreate({title: 'lulwat'});
     expect(action.type).toEqual('STATEMENT_CREATE');
-    console.log(action);
     expect(action.payload.id).toBeTruthy();
     expect(action.payload.timestamp).toBeTruthy();
     expect(action.payload.title).toBe('lulwat');
@@ -16,7 +15,6 @@ describe('testing statement actions', () => {
   test('statementUpdate returns a STATEMENT_UPDATE action', () => {
     let mockStatement = {id: '123', timestamp: new Date() , title: 'lulwat'};
     let action = statementUpdate(mockStatement);
-    console.log(action);
     expect(action).toEqual({
       type: 'STATEMENT_UPDATE',
       payload: mockStatement,
@@ -25,7 +23,6 @@ describe('testing statement actions', () => {
   test('statementDelete returns a STATEMENT_DELETE action', () => {
     let mockStatement = {id: '124', timestamp: new Date() , title: 'cool'};
     let action = statementDelete(mockStatement);
-    console.log(statementDelete());
     expect(action).toEqual({
       type: 'STATEMENT_DELETE',
       payload: mockStatement,
