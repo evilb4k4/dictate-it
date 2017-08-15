8292cab11219c78b605c0d85df0de12e172b0ebf
Ryan Turner <ryanthepit@gmail.com>
2017-08-15 11:59:31 -0700

    built frontend routes to redirect

--- frontend/src/component/app/index.js
+++ frontend/src/component/app/index.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import {connect} from 'react-redux';
-// import LandingContainer from '../landing-container';
 import Dictation from '../dictation';
 import {MemoryRouter, Switch, Route} from 'react-router-dom';
+import * as util from '../../lib/util.js';
+import * as auth from '../../action/auth.js';
+import * as route from '../../action/route.js';
 
-class App extends React.Component {
+import LandingContainer from '../landing-container';
+import SignupContainer from '../signup-container';
 
-  render() {
+export class App extends React.Component {
+
+  render(){
     return (
-      <main>
-        <header>
-          <h1>Dictation It!</h1>
-          <button onClick={this.props.goToLanding}>Home</button>
-        </header>
-        <Dictation />
-      </main>
+      <div className='app'>
+        <main>
+          <header>
+            <h1> Dictate It! </h1>
+            <button onClick={this.props.goToLanding}>Home</button>
+          </header>
+          <Dictation />
+        </main>
+
+        <MemoryRouter>
+          <Switch location={{pathname: this.props.route}}>
+            <Route exact path='/signup' component={SignupContainer} />
+            <Route exact path='/landing' component={LandingContainer} />
+            <Route exact path='/login' component={() => <p> Login </p>} />
+          </Switch>
+        </MemoryRouter>
+      </div>
     );
   }
 }
 
-export default App;
+let mapStateToProps = (state) => ({
+  token: state.token,
+  route: state.route,
+});
 
-//<Route path='/landing' component={Landing} />
+let mapDispatchToProps = (dispatch) => ({
+  login: (token) => dispatch(auth.login(token)),
+  goToLanding: () => dispatch(route.switchRoute('/landing')),
+  goToLogin: () => dispatch(route.switchRoute('/login')),
+  goToSignup: () => dispatch(route.switchRoute('/signup')),
+});
 
-// <MemoryRouter>
-// <Switch location={{ pathname: this.props.route }}>
-// <Route exact path='/dictation' component={Dictation} />
-// </Switch>
-// </MemoryRouter>
+export default connect(mapStateToProps, mapDispatchToProps)(App);
