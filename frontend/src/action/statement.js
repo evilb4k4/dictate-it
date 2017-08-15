f51c6e392c93a39c6a3848f470f5fe9c5c5c9723
adan.rabadan <evilb4k4@gmail.com>
2017-08-15 13:58:07 -0700

    created test for statement action for create, update and delete

--- frontend/src/action/statement.js
+++ frontend/src/action/statement.js
@@ -2,7 +2,7 @@ import uuid from 'uuid/v1';
 
 export const statementCreate = (statement) => ({
   type: 'STATEMENT_CREATE',
-  payload: { ...statement, id: uuid() },
+  payload: { ...statement, id: uuid(), timestamp: new Date()},
 });
 export const statementUpdate = (statement) => ({
   type: 'STATEMENT_UPDATE',
