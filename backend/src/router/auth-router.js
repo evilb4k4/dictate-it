0a052306200fa341623ce347780e92cc7df08969
Izabella Baer <izabellabaer@gmail.com>
2017-08-15 12:54:21 -0700

    added statement model and dictation router

--- backend/src/router/auth-router.js
+++ backend/src/router/auth-router.js
@@ -65,34 +65,3 @@ export default new Router()
       })
       .catch(next);
   });
-
-
-
-
-
-// authRouter.post('/api/auth/register', jsonParser, (req, res, next) => {
-//   console.log('body', req.body);
-//   if(!req.body.password || !req.body.username) {
-//     return next(new Error('required arguments'));
-//   }
-//
-//   User.create(req.body)
-//     .then(token => res.send(token))
-//     .catch(next);
-// });
-//
-// authRouter.get('/api/auth/login', (req, res, next) => {
-//   console.log('api/auth/login');
-//   req.user.tokenCreate()
-//     .then(token => res.send(token))
-//     .catch(next);
-// });
-//
-// authRouter.delete('api/auth/delete', (req, res, next) => {
-//   console.log('Hit DELETE', req.user);
-//   User.findOneAndRemove({_id: req.user._id})
-//     .then(user => {
-//       res.send(user._id);
-//     })
-//     .catch(next);
-// });
