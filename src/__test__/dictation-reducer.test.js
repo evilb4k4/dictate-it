c4b00bf7866fd48c5e8bbf6c58aae9122a1047f3
adan.rabadan <evilb4k4@gmail.com>
2017-08-16 11:58:51 -0700

    added test for dictation-reducer

--- src/__test__/dictation-reducer.test.js
+++ src/__test__/dictation-reducer.test.js
@@ -12,7 +12,71 @@ describe('testing dictation reducer', () => {
     ];
 
     let result = dictationReducer(mockState, {type: null});
-    console.log(result);
     expect(result).toEqual(mockState);
+  });
+  test('DICTATION_CREATE should append to the array', () => {
+    let actionOne = {
+      type: 'DICTATION_CREATE',
+      payload: {
+        id:'123',
+        title: 'cool beans',
+        timestamp: new Date(),
+      },
+    };
+
+    let state = dictationReducer([], actionOne);
+    expect(state.length).toBe(1);
+    expect(state[0]).toBe(actionOne.payload);
+
+    let actionTwo = {
+      type: 'DICTATION_CREATE',
+      payload: {
+        id:'goo',
+        title: 'lulwat',
+        timestamp: new Date(),
+      },
+    };
+
+    state = dictationReducer(state, actionTwo);
+    expect(state.length).toBe(2);
+    expect(state[0]).toBe(actionOne.payload);
+    expect(state[1]).toBe(actionTwo.payload);
+  });
+  test('DICTATION_DELETE should delete a dictation from the array', () => {
+    let mockState = [
+      { id: 'abc',  title: 'cool', timestamp: new Date()},
+      { id: '123',  title: 'numbers', timestamp: new Date()},
+      { id: 'zyx',  title: 'lulwat', timestamp: new Date()},
+      { id: '000',  title: 'zero', timestamp: new Date()},
+    ];
+
+    let actionOne= {
+      type: 'DICTATION_DELETE',
+      payload: mockState[1],
+    };
+
+    let state = dictationReducer(mockState, actionOne);
+
+    expect(state.length).toBe(3);
+    expect(state).toEqual(mockState.filter(item => item.id != '123'));
+  });
+  test('DICTATION_UPDATE should update a dictation in the array', () => {
+    let mockState = [
+      { id: 'abc',  title: 'cool', timestamp: new Date()},
+      { id: '123',  title: 'numbers', timestamp: new Date()},
+      { id: 'zyx',  title: 'lulwat', timestamp: new Date()},
+      { id: '000',  title: 'zero', timestamp: new Date()},
+    ];
+
+    let actionOne= {
+      type: 'DICTATION_UPDATE',
+      payload: {id: 'zyx', title: 'sub', timestamp: new Date()},
+    };
+
+    let state = dictationReducer(mockState, actionOne);
+
+    expect(state.length).toBe(4);
+    expect(state).toEqual(mockState.map(item =>
+      item.id == 'zyx' ? actionOne.payload : item));
   });
 });
