3451ad2c1e5bc199f79de217571c96e9b3f25749
Ryan Turner <ryanthepit@gmail.com>
2017-08-17 16:35:12 -0700

    combined divs on signup

--- src/component/login-container/index.js
+++ src/component/login-container/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as util from '../../lib/util';
 import * as auth from '../../action/auth-actions.js';
-import {Redirect} from 'react-router-dom';
+import {Redirect, Link} from 'react-router-dom';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import * as querystring from 'querystring';
 
 
 export class LoginContainer extends React.Component {
@@ -37,11 +37,40 @@ export class LoginContainer extends React.Component {
   }
 
   render(){
-    const style = {
+    const styles = {
 
-    };
+  };
+
+let googleLoginBaseURL='https://accounts.google.com/o/oauth2/v2/auth';
+let googleLoginQuery = querystring.stringify({
+  client_id: __GOOGLE_CLIENT_ID__,
+  response_type: 'code',
+  redirect_uri: `${__API_URL__}/oauth/google/code`,
+  scope: 'openid profile email',
+  prompt: __DEBUG__ ? 'consent' : undefined,
+});
+let googleLoginURL = `${googleLoginBaseURL}?${googleLoginQuery}`;
+
     return(
       <div className='login-container'>
+      <header>
+        <h1> Dictate It! </h1>
+        {util.renderIf(this.props.token,
+          <p>
+            <Link to='/landing'>Home</Link>
+            <Link to='/dictation'>New Dictation</Link>
+            <button onClick={this.props.logout}>Logout</button>
+          </p>
+        )}
+        {util.renderIf(!this.props.token,
+          <p>
+            <Link to='/'>Login</Link>
+            <a href={googleLoginURL}>Login with Google</a>
+            <Link to='/signup'>Sign Up</Link>
+          </p>
+        )}
+      </header>
+
         {util.renderIf(this.props.token,
           <Redirect to='/landing' />
         )}
@@ -62,13 +91,11 @@ export class LoginContainer extends React.Component {
             value={this.state.password}
             onChange={this.handleChange}
           />
-
           <MuiThemeProvider>
-          <div class='login-submit'>
+          <div className='login-submit'>
           <RaisedButton type='submit' label="Submit" fullWidth={true} />
           </div>
           </MuiThemeProvider>
-
         </form>
       </div>
     );
