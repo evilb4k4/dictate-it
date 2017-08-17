6f7c6f273a83e328aaaf79d3b6bae84020534daa
Izabella Baer <izabellabaer@gmail.com>
2017-08-17 16:41:29 -0700

    added material ui

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import * as querystring from 'querystring';
 import {BrowserRouter, Link, Route} from 'react-router-dom';
 import * as util from '../../lib/util.js';
 import * as auth from '../../action/auth-actions.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {cyan500} from 'material-ui/styles/colors';
 
 import Dictation from '../dictation';
 import LandingContainer from '../landing-container';
@@ -20,44 +22,31 @@ export class App extends React.Component {
   }
 
   render(){
-    let googleLoginBaseURL='https://accounts.google.com/o/oauth2/v2/auth';
-    let googleLoginQuery = querystring.stringify({
-      client_id: __GOOGLE_CLIENT_ID__,
-      response_type: 'code',
-      redirect_uri: `${__API_URL__}/oauth/google/code`,
-      scope: 'openid profile email',
-      prompt: __DEBUG__ ? 'consent' : undefined,
+    const muiTheme = getMuiTheme({
+      palette: {
+        textColor: cyan500,
+      },
+      appBar: {
+        height: 50,
+      },
     });
 
-    let googleLoginURL = `${googleLoginBaseURL}?${googleLoginQuery}`;
     return (
-      <div className='app'>
-        <BrowserRouter>
-          <main>
-            <header>
-              <h1> Dictate It! </h1>
-              {util.renderIf(this.props.token,
-                <p>
-                  <Link to='/landing'>Home</Link>
-                  <Link to='/dictation'>New Dictation</Link>
-                  <button onClick={this.props.logout}>Logout</button>
-                </p>
-              )}
-              {util.renderIf(!this.props.token,
-                <p>
-                  <Link to='/'>Login</Link>
-                  <a href={googleLoginURL}>Login with Google</a>
-                  <Link to='/signup'>Sign Up</Link>
-                </p>
-              )}
-            </header>
-            <Route exact path='/' component={LoginContainer} />
-            <Route exact path='/signup' component={SignupContainer} />
-            <Route exact path='/landing' component={LandingContainer} />
-            <Route exact path='/dictation' component={Dictation} />
-          </main>
-        </BrowserRouter>
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div className='app'>
+          <BrowserRouter>
+            <main>
+
+              <Route exact path='/' component={LoginContainer} />
+              <Route exact path='/signup' component={SignupContainer} />
+              <Route exact path='/landing' component={LandingContainer} />
+              <Route exact path='/dictation' component={Dictation} />
+              <Route exact path='/dictation/*' component={Dictation} />
+
+            </main>
+          </BrowserRouter>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/component/login-container/index.js
+++ src/component/login-container/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as util from '../../lib/util';
 import * as auth from '../../action/auth-actions.js';
-import {Redirect} from 'react-router-dom';
+import * as querystring from 'querystring';
+import {Redirect, Link} from 'react-router-dom';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import {cyan500} from 'material-ui/styles/colors';
+import TextField from 'material-ui/TextField';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 export class LoginContainer extends React.Component {
   constructor(props) {
@@ -37,38 +40,56 @@ export class LoginContainer extends React.Component {
   }
 
   render(){
-    const style = {
+    let googleLoginBaseURL='https://accounts.google.com/o/oauth2/v2/auth';
+    let googleLoginQuery = querystring.stringify({
+      client_id: __GOOGLE_CLIENT_ID__,
+      response_type: 'code',
+      redirect_uri: `${__API_URL__}/oauth/google/code`,
+      scope: 'openid profile email',
+      prompt: __DEBUG__ ? 'consent' : undefined,
+    });
 
-    };
+    let googleLoginURL = `${googleLoginBaseURL}?${googleLoginQuery}`;
     return(
       <div className='login-container'>
+        <header>
+          <h1> Dictate It </h1>
+          {util.renderIf(this.props.token,
+            <p>
+              <Link to='/landing'>Home</Link>
+              <Link to='/dictation'>New Dictation</Link>
+              <button onClick={this.props.logout}>Logout</button>
+            </p>
+          )}
+          {util.renderIf(!this.props.token,
+            <div className='login'>
+              <Link to='/'>Login</Link>
+              <div className="g-signin2" data-onsuccess="onSignIn"><a href={googleLoginURL}>
+                  Login with Google</a></div>
+              <Link to='/signup'>Sign Up</Link>
+            </div>
+          )}
+        </header>
         {util.renderIf(this.props.token,
           <Redirect to='/landing' />
         )}
         <form className='login-form' onSubmit={this.handleSubmit}>
-          <input
-            required
+          <TextField
             name='username'
-            type='text'
-            placeholder='username'
+            hintText='username'
             value={this.state.username}
             onChange={this.handleChange}
           />
-          <input
-            required
+          <TextField
             name='password'
-            type='password'
-            placeholder='password'
+            hintText='password'
             value={this.state.password}
             onChange={this.handleChange}
           />
 
-          <MuiThemeProvider>
-          <div class='login-submit'>
-          <RaisedButton type='submit' label="Submit" fullWidth={true} />
+          <div className='login-submit'>
+            <RaisedButton type='submit' label="Submit" fullWidth={true} />
           </div>
-          </MuiThemeProvider>
-
         </form>
       </div>
     );
